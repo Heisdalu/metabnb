30dcaa5f9ded12f6e31c69163f2395d61351a1da
divine <divineobi@gmail.com>
2022-11-13 00:08:27 +0100

    chore: add header grid pic webp format

--- src/components/Header/Illustration.jsx
+++ src/components/Header/Illustration.jsx
@@ -1,11 +1,12 @@
 import style from "./Illustration.module.css";
 import headerGrid from "../../../public/assets/img/header-grid.png";
+import headerGridWebp from "../../../public/assets/img/header-grid.png";
 
 const Illustration = () => {
   return (
     <figure className={style.headerPicBox}>
       <picture>
-        <source srcset="" type="image/webp" />
+        <source srcset={headerGridWebp} type="image/webp" />
         <img src={headerGrid} alt="" />
       </picture>
     </figure>
