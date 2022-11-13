8d7abe507ac987552c63d6b889fc24b5af1179df
divine <divineobi@gmail.com>
2022-11-13 18:23:10 +0100

    feat: add data folder

---
+++ src/data/HomeData.js
@@ -0,0 +1,19 @@
+import brownPic from "../../public/assets/img/brown.png";
+import hutPic from "../../public/assets/img/home.png";
+import mansionPic from "../../public/assets/img/mansion.png";
+import homePic from "../../public/assets/img/home.png";
+import upstairsPic from "../../public/assets/img/upstairs.png";
+import volcanoPic from "../../public/assets/img/volcano-home.png";
+import islandPic from "../../public/assets/img/island.png";
+import castlePic from "../../public/assets/img/castle.png";
+
+export const images = [
+  brownPic,
+  hutPic,
+  mansionPic,
+  homePic,
+  upstairsPic,
+  volcanoPic,
+  islandPic,
+  castlePic,
+];
