cf5a8dbdfc67c7323a84ad6bc60adffcd6e1c767
divine <divineobi@gmail.com>
2022-11-14 00:49:32 +0100

    feat: add more images

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import brownPic from "../../public/assets/img/brown.png";
+import hutPic from "../../public/assets/img/home.png";
+import mansionPic from "../../public/assets/img/mansion.png";
+import homePic from "../../public/assets/img/home.png";
+import upstairsPic from "../../public/assets/img/upstairs.png";
+import volcanoPic from "../../public/assets/img/volcano-home.png";
+import islandPic from "../../public/assets/img/island.png";
+import castlePic from "../../public/assets/img/castle.png";
+import circusPic from "../../public/assets/img/circus.png";
+import knightPic from "../../public/assets/img/knight.png";
+import babyHomePic from "../../public/assets/img/baby-home.png";
+import iceHomePic from "../../public/assets/img/ice-home.png";
+import clubPic from "../../public/assets/img/club.png";
+import playPark from "../../public/assets/img/play-park.png";
+import richHome from "../../public/assets/img/rich-home.png";
+import candyHome from "../../public/assets/img/candy-home.png";
+
+export const dataImages = [
+  upstairsPic,
+  volcanoPic,
+  islandPic,
+  castlePic,
+  brownPic,
+  hutPic,
+  mansionPic,
+  homePic,
+  circusPic,
+  knightPic,
+  babyHomePic,
+  iceHomePic,
+  clubPic,
+  playPark,
+  richHome,
+  candyHome,
+];
