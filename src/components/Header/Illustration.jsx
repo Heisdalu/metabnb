9a39968dd666ddc431bf11cb85c27b1a38475976
divine <divineobi@gmail.com>
2022-11-15 10:11:50 +0100

    styles: styles on header

--- src/components/Header/Illustration.jsx
+++ src/components/Header/Illustration.jsx
@@ -7,7 +7,13 @@ const Illustration = () => {
     <figure className={style.headerPicBox}>
       <picture>
         <source srcSet={headerGridWebp} type="image/webp" />
-        <img src={headerGrid} alt="" />
+        <img
+          src={headerGrid}
+          width="300px"
+          height="350px"
+          style={{ width: "100%", height: "auto" }}
+          alt="nft images"
+        />
       </picture>
     </figure>
   );
